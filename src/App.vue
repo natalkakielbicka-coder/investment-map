df654c2954778e06aad223bde43736392b4b58bb
nkpiksalia <natalkakielbicka@gmail.com>
2026-09-20 06:37:17 +0200

    Add Leaflet map component

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import InvestmentMap from './components/InvestmentMap.vue'
+</script>
+
+<template>
+  <main>
+    <h1>Mapa okolicy inwestycji</h1>
+    <InvestmentMap />
+  </main>
+</template>
---
+++ src/components/InvestmentMap.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+const mapContainer = ref(null)
+
+onMounted(() => {
+  const map = L.map(mapContainer.value).setView([52.2297, 21.0122], 13)
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors',
+  }).addTo(map)
+})
+</script>
+
+<template>
+  <div ref="mapContainer" class="map"></div>
+</template>
+
+<style scoped>
+.map {
+  height: 500px;
+  width: 100%;
+}
+</style>
